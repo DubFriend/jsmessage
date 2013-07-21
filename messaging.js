75c98c1c6aee937f3ccad949a560c84029663259
Brian Detering <bdeterin@gmail.com>
2013-07-21 01:19:33 -0700

    copied in underscore subset and removed underscore as dependency

--- messaging.js
+++ messaging.js
@@ -1,109 +1,216 @@
-var messaging = {};
+var messaging = (function () {
+    // Underscore Subset, renamed to "lib" namespace.
+    //     Underscore.js 1.5.1
+    //     http://underscorejs.org
+    //     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+    //     Underscore may be freely distributed under the MIT license.
 
+    // Establish the object that gets returned to break out of a loop iteration.
+    var breaker = {};
 
-messaging.mixinPubSub = function (object) {
-    object = object || {};
-    var subscribers = {};
+    // Save bytes in the minified (but not gzipped) version:
+    var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
 
-    object.subscribe = function (callback, topic) {
-        subscribers[topic] = subscribers[topic] || [];
-        subscribers[topic].push(callback);
+    // Create quick reference variables for speed access to core prototypes.
+    var push             = ArrayProto.push,
+        slice            = ArrayProto.slice,
+        concat           = ArrayProto.concat,
+        toString         = ObjProto.toString,
+        hasOwnProperty   = ObjProto.hasOwnProperty;
+
+    // All **ECMAScript 5** native function implementations that we hope to use
+    // are declared here.
+    var nativeForEach      = ArrayProto.forEach,
+        //nativeMap          = ArrayProto.map,
+        //nativeReduce       = ArrayProto.reduce,
+        //nativeReduceRight  = ArrayProto.reduceRight,
+        nativeFilter       = ArrayProto.filter;
+        //nativeEvery        = ArrayProto.every,
+        //nativeSome         = ArrayProto.some,
+        //nativeIndexOf      = ArrayProto.indexOf,
+        //nativeLastIndexOf  = ArrayProto.lastIndexOf,
+        //nativeIsArray      = Array.isArray,
+        //nativeKeys         = Object.keys,
+        //nativeBind         = FuncProto.bind;
+
+    var lib = {};
+
+    // The cornerstone, an `each` implementation, aka `forEach`.
+    // Handles objects with the built-in `forEach`, arrays, and raw objects.
+    // Delegates to **ECMAScript 5**'s native `forEach` if available.
+    lib.each = function (obj, iterator, context) {
+        if (obj == null) {
+            return;
+        }
+        if (nativeForEach && obj.forEach === nativeForEach) {
+            obj.forEach(iterator, context);
+        }
+        else if (obj.length === +obj.length) {
+            for (var i = 0, l = obj.length; i < l; i++) {
+                if (iterator.call(context, obj[i], i, obj) === breaker) {
+                    return;
+                }
+            }
+        }
+        else {
+            for (var key in obj) {
+                if (lib.has(obj, key)) {
+                    if (iterator.call(context, obj[key], key, obj) === breaker) {
+                        return;
+                    }
+                }
+            }
+        }
     };
 
-    object.unsubscribe = function (subscriber, topic) {
-        var filter = function (topic) {
-            return _.filter(subscribers[topic], function (callback) {
-                return callback !== subscriber;
-            });
+    // Return all the elements that pass a truth test.
+    // Delegates to **ECMAScript 5**'s native `filter` if available.
+    // Aliased as `select`.
+    //_.filter = _.select = function(obj, iterator, context) {
+    lib.filter = function (obj, iterator, context) {
+        var results = [];
+        if (obj == null) {
+            return results;
+        }
+        if (nativeFilter && obj.filter === nativeFilter) {
+            return obj.filter(iterator, context);
+        }
+        lib.each(obj, function(value, index, list) {
+            if (iterator.call(context, value, index, list)) {
+                results.push(value);
+            }
+        });
+        return results;
+    };
+
+    // Optimize `isFunction` if appropriate.
+    if (typeof (/./) !== 'function') {
+        lib.isFunction = function(obj) {
+            return typeof obj === 'function';
+        };
+    }
+
+    // Shortcut function for checking if an object has a given property directly
+    // on itself (in other words, not on a prototype).
+    lib.has = function(obj, key) {
+        return hasOwnProperty.call(obj, key);
+    };
+
+
+
+    //--------- end underscore subset ------------------------------------------
+
+
+
+
+    var messaging = {};
+
+    messaging.mixinPubSub = function (object) {
+        object = object || {};
+        var subscribers = {};
+
+        object.subscribe = function (callback, topic) {
+            subscribers[topic] = subscribers[topic] || [];
+            subscribers[topic].push(callback);
         };
 
-        if(topic) {
-            if(subscribers[topic]) {
-                subscribers[topic] = filter(topic);
+        object.unsubscribe = function (subscriber, topic) {
+            var filter = function (topic) {
+                return lib.filter(subscribers[topic], function (callback) {
+                    return callback !== subscriber;
+                });
+            };
+
+            if(topic) {
+                if(subscribers[topic]) {
+                    subscribers[topic] = filter(topic);
+                }
+                else {
+                    throw "topic not found";
+                }
             }
             else {
-                throw "topic not found";
+                lib.each(subscribers, function (values, topic) {
+                    subscribers[topic] = filter(topic);
+                });
             }
-        }
-        else {
-            _.each(subscribers, function (values, topic) {
-                subscribers[topic] = filter(topic);
-            });
-        }
-    };
+        };
 
-    object.publish = function (data, topic) {
-        if(topic) {
-            _.each(subscribers[topic], function (callback) {
-                callback(data);
-            });
-        }
-        else {
-            _.each(subscribers, function (values, topic) {
-                _.each(values, function (callback) {
+        object.publish = function (data, topic) {
+            if(topic) {
+                lib.each(subscribers[topic], function (callback) {
                     callback(data);
                 });
-            });
-        }
+            }
+            else {
+                lib.each(subscribers, function (values, topic) {
+                    lib.each(values, function (callback) {
+                        callback(data);
+                    });
+                });
+            }
+        };
+
+        return object;
     };
 
-    return object;
-};
 
+    //  example
+    //  var object = messaging.mixinEvents(object, {
+            //passed return value of called method, and "this"
+            //is bound to the object receiving the mixin.
+    //      functionName: function (returnValue) {
+                //what is returned is what gets passed to the user supplied callback.
+    //          return {
+    //              foo: returnValue,
+    //              bar: this.objectProperty
+    //          };
+    //      }
+    //  });
+    messaging.mixinEvents = function (object, argumentGenerators) {
+        var bindings = {},
+            argGen = argumentGenerators || {};
 
-//  example
-//  var object = messaging.mixinEvents(object, {
-        //passed return value of called method, and "this"
-        //is bound to the object receiving the mixin.
-//      functionName: function (returnValue) {
-            //what is returned is what gets passed to the user supplied callback.
-//          return {
-//              foo: returnValue,
-//              bar: this.objectProperty
-//          };
-//      }
-//  });
-messaging.mixinEvents = function (object, argumentGenerators) {
-    var bindings = {},
-        argGen = argumentGenerators || {};
+        //wrap each function of the object with its trigger.
+        lib.each(object, function (property, name) {
+            if(lib.isFunction(property)) {
+                object[name] = function () {
+                    var callbackArg,
+                        returnValue = property.apply(object, arguments);
 
-    //wrap each function of the object with its trigger.
-    _.each(object, function (property, name) {
-        if(_.isFunction(property)) {
-            object[name] = function () {
-                var callbackArg,
-                    returnValue = property.apply(object, arguments);
+                    if(bindings[name]) {
+                        if(argGen[name]) {
+                            callbackArg = argGen[name].apply(object, [returnValue]);
+                        }
 
-                if(bindings[name]) {
-                    if(argGen[name]) {
-                        callbackArg = argGen[name].apply(object, [returnValue]);
+                        lib.each(bindings[name], function (callback) {
+                            callback(callbackArg);
+                        });
                     }
 
-                    _.each(bindings[name], function (callback) {
-                        callback(callbackArg);
-                    });
-                }
+                    return returnValue;
+                };
+            }
+        });
 
-                return returnValue;
-            };
-        }
-    });
+        object.on = function (event, callback) {
+            bindings[event] = bindings[event] || [];
+            bindings[event].push(callback);
+        };
 
-    object.on = function (event, callback) {
-        bindings[event] = bindings[event] || [];
-        bindings[event].push(callback);
+        object.off = function (event, callback) {
+            if(callback) {
+                bindings[event] = lib.filter(bindings[event], function (subscriber) {
+                    return subscriber !== callback;
+                });
+            }
+            else {
+                bindings[event] = undefined;
+            }
+        };
+
+        return object;
     };
 
-    object.off = function (event, callback) {
-        if(callback) {
-            bindings[event] = _.filter(bindings[event], function (subscriber) {
-                return subscriber !== callback;
-            });
-        }
-        else {
-            bindings[event] = undefined;
-        }
-    };
-
-    return object;
-};
+    return messaging;
+}());
